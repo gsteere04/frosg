86eae85820698f1f54cd646541e87dfc786aab23
Grant <gsteere4717@xmail.dixietech.edu>
2024-08-21 11:15:46 -0600

    create Footer.tsx component

---
+++ frosg/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+    return (
+        <footer>
+            <p>&copy; 2024 First Responders of St. George.  All rights reserved.</p>
+        </footer>
+    );
+};
+
+export default Footer;
